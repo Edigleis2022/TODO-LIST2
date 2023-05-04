a8cd9a5b5bf1518f8fd5ccb332bbc15f851af879
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2023-05-03 22:51:23 -0400

    🐛 Layout com bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,41 @@ const darkTheme = createTheme({
 });
 
 function App() {
-const theme = useTheme();
+  const theme = useTheme();
 
   return (
-     <>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <AppBar position='static'>
-        <Toolbar sx={{
-            paddingTop:theme.spacing(2),
-            paddingBottom:theme.spacing(2),
-            display:"flex",
-            justifyContent:'cente',
-            alignItem:'center',
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <AppBar position='static'>
+          <Toolbar sx={{
+            paddingTop: theme.spacing(2),
+            paddingBottom: theme.spacing(2),
+            display: "flex",
+            justifyContent: 'cente',
+            alignItem: 'center',
             '@media all': {
               minHeight: 200,
             }
-        }} >
-          <Typography variant="h5" componet="h1">
-            Todo
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-         <Container>
-        <Grid container spacing={theme.spacing(0.5)} >
+          }} >
+            <Typography variant="h5" component="h1" >
+              Todo
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Container>
+            <Grid container spacing={theme.spacing(0.5)} >
               <Grid item xl={6} sm={12}>
-                  <TextField name='task' fullWidth/>
+                <TextField name='task' fullWidth />
               </Grid>
-              <Grid xl={6} sm={12}>
-                 <Button variant="contained">Criar</Button>
+              <Grid  item xl={6} sm={12}>
+                <Button variant="contained" fullWidth >Criar</Button>
               </Grid>
-        </Grid>
-        </Container>
-      </main>
-    </ThemeProvider>
+            </Grid>
+          </Container>
+        </main>
+      </ThemeProvider>
     </>
   );
 }
