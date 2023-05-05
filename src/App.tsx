9b9ffb675a3bbc017f537aa9a8419842f7b05035
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2023-05-04 22:15:25 -0400

    ⚡️ Correção de posição da Layout

--- src/App.tsx
+++ src/App.tsx
@@ -29,64 +29,65 @@ function App() {
             }
           }} >
             <Typography variant="h5" component="h1" sx={{
-              display:'flex',
-              alignItems:'center',
-              gap:theme.spacing(2),
-              color:theme.palette.primary.light
+              display: 'flex',
+              alignItems: 'center',
+              gap: theme.spacing(2),
+              color: theme.palette.primary.light
 
             }} >
-              <CheckFat size={32} weight='fill'/> Lista de Tarefas
+              <CheckFat size={32} weight='fill' /> Lista de Tarefas
             </Typography>
           </Toolbar>
         </AppBar>
 
 
         <main>
-        <Container sx={{
-          position:'relative'
+          <Container sx={{
+            position: 'relative'
 
-        }}>
+          }}>
             <Grid container spacing={theme.spacing(0.5)} sx={{
               position: 'absolute',
               top: '-27'
             }}>
               <Grid item xl={10} sm={12}>
-                <TextField  
-                placeholder='Adicione uma nova tarefa'
-                variant='outlined' 
-                name='task' fullWidth 
-                
-                sx={{
-                  backgroundColor:colors.grey[800]
-                }} />
+                <TextField
+                  placeholder='Adicione uma nova tarefa'
+                  variant='outlined'
+                  name='task' fullWidth
+
+                  sx={{
+                    backgroundColor: colors.grey[800]
+                  }} />
               </Grid>
 
-              <Grid  item xl={2} sm={12}>
+              <Grid item xl={2} sm={12}>
                 <Button variant="contained" fullWidth sx={{
-                  height:'100%'
-                }}> 
-                <span>Criar </span><PlusCircle size={32}/> 
+                  height: '100%'
+                }}>
+                  <span>Criar </span><PlusCircle size={32} />
                 </Button>
               </Grid>
 
-            
 
-            <Grid container spacing={theme.spacing(1)} sx={{
-              marginTop: '25px'
-            }} >
-              <Grid item >
-                <Typography variant='caption' sx={{
-                  display:'flex',
-                  alignItems:'center',
-                  gap: theme.spacing(2),
-                  paddingLeft: '5px'
-                }} >
-                  <span>Tarefas Criadas</span>
-                  <Badge color='primary' badgeContent={0} showZero></Badge> 
-                </Typography>
+
+              <Grid container spacing={theme.spacing(1)} sx={{
+                marginTop: '25px'
+              }} >
+                <Grid item >
+                  <Typography variant='caption' sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: theme.spacing(2),
+                    paddingLeft: '5px'
+                  }} >
+                    <span>Tarefas Criadas</span>
+                    <Badge color='primary' badgeContent={0} showZero></Badge>
+                  </Typography>
+                </Grid>
+
               </Grid>
 
-            </Grid>
             </Grid>
 
 
