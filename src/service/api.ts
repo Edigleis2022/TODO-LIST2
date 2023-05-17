15adc5bb2ebb7ed1ebb4643e4607c75dcfeba72c
Edigleis2022 <edigleis_2007@hotmail.com>
2023-05-17 16:21:24 -0400

    ✨Novas Funções de Layout

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,28 +1,34 @@
-import axios from  'axios';
-
-const api = axios.create({
+import axios from 'axios';
+import { Task} from '../Types'
+ 
+export const api = axios.create({
     baseURL: 'http://localhost:3000',
     timeout: 3000,
 });
 
 export async function getAll() {
-   
+    const tasks = await api.get('tasks')
+    return tasks.data
 }
-export async function getById(id:number) {
-   const tasks = await api.get(`tasks/${id}`)
-   return tasks.data
+export async function getById(id: number) {
+    const tasks = await api.get(`tasks/${id}`)
+    return tasks.data
 }
 //Salvar Registro
-export async function save(task:Task) {
+export async function save(task: Task) {
     const response = await api.post('tasks', {
         ...task
     })
     return response.data
 }
-//Atualizat Registro
-export async function update(task:Task) {
+//Atualiza Registro
+export async function update(task: Task) {
     const response = await api.put(`task/${task.id}`, {
         ...task
     })
     return response.data
+}
+export async function exclude(id: number){
+    const tasks = await api.delete(`tasks/${id}`,
+        return tasks.data
 }
